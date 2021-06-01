a9e7b6c8fe4abe3be2787c51cf64d47439a0febf
Hongyang Lin <linhy@terpmail.umd.edu>
2021-06-01 16:30:32 +0800

    updated

--- static/JS/scale.fix.js
+++ static/JS/scale.fix.js
@@ -25,3 +25,27 @@
         document.addEventListener("touchend", gestureEnd, false);
     }
 })(document);
+
+var navOpen = false
+
+function nav() {
+    if (navOpen) {
+        closeNav()
+    } else {
+        openNav()
+    }
+}
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = `200px`;
+    document.getElementById("main").style.marginLeft = `200px`;
+    document.getElementsByClassName("openbtn")[0].style.background = "#f1f1f1";
+    navOpen = true
+}
+    
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.getElementsByClassName("openbtn")[0].style.background = "white";
+    navOpen = false
+}
